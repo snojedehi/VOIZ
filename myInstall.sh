66c1f8b812a82fa18fd9cdbeeffd06b7ee864053
macbook <shayan.noje@gmail.com>
2024-02-11 17:57:51 +0330

    re

--- myInstall.sh
+++ myInstall.sh
@@ -1,16 +1,3 @@
 #!/bin/bash
 
-function bulkdids(){
-if [ ! -d "/var/www/html/admin/modules/bulkdids" ]; then
-#BULK DIDs Module
 yes | cp -rf issabelpbxmodules/bulkdids /var/www/html/admin/modules/
-amportal a ma install bulkdids
-fi
-    echo "**Bulk DIDs Module Added." >> voiz-installation.log
-}
-
-##Install Bulk DIDs Module
-bulkdids
-
-  COUNTER=$(($COUNTER+10))
-    echo ${COUNTER} 
