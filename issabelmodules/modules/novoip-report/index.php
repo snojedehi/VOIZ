ab589d49b47a61cb3f6ea45ca95d1b2b9e2914cd
root <aaddminShayan22>
2024-02-13 17:03:28 +0330

    db

--- issabelmodules/modules/novoip-report/index.php
+++ issabelmodules/modules/novoip-report/index.php
@@ -65,11 +65,8 @@ function viewFormSoftphones($smarty, $module_name, $local_templates_dir, $arrCon
         } else {
             $sql = "SELECT * FROM `trunks`";
             $recordset = $pDB->fetchTable($sql, TRUE, array($extension, $extension, MAX_CALL_RECORDS));
-            // $tunks=Array()
-            foreach ($recordset as $tupla) {
-                
-                // array_push($tunks,$tupla['name'])
-            }
+            $tunks=Array()
+            
             $smarty->assign("trunks", "2");
         }
     $smarty->assign("icon",  "modules/$module_name/images/softphones.png");
