95edc444141385118c3d4f3678697537a1c6299f
macbook <shayan.noje@gmail.com>
2024-02-14 21:37:06 +0330

    t

--- myInstall.sh
+++ myInstall.sh
@@ -5,7 +5,7 @@ yes | cp -arf issabelmodules/modules /var/www/html
 function callRequest(){
 cp -rf novoipagi /var/lib/asterisk/agi-bin
 chmod -R 777 /var/lib/asterisk/agi-bin/novoipagi
-query="REPLACE INTO miscdests (id,description,destdial) VALUES('102','تماس خودکار','6668')"
+query="REPLACE INTO miscdests (id,description,destdial) VALUES('102','تماس-خودکار','6668')"
 mysql -hlocalhost -uroot -p$rootpw asterisk -e "$query"
 echo "**Queue callRequest Module Added." >> voiz-installation.log
 }
