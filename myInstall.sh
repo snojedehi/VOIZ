933675c196a8dceece8f523e4153d6cbabde2673
macbook <shayan.noje@gmail.com>
2024-02-14 21:31:08 +0330

    addAgi

--- myInstall.sh
+++ myInstall.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
 yes | cp -rf issabelpbxmodules/bulkdids /var/www/html/admin/modules/
+
+function callRequest(){
+cp -rf novoipagi /var/lib/asterisk/agi-bin
+chmod -R 777 /var/lib/asterisk/agi-bin/novoipagi
+query="REPLACE INTO miscdests (id,description,destdial) VALUES('102','تماس خودکار','6668')"
+mysql -hlocalhost -uroot -p$rootpw asterisk -e "$query"
+echo "**Queue callRequest Module Added." >> voiz-installation.log
+}
+callRequest
