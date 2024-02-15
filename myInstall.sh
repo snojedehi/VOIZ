5e291f1a9b3f4675f76371bf08987bd0791fc014
macbook <shayan.noje@gmail.com>
2024-02-15 17:31:45 +0330

    cron

--- myInstall.sh
+++ myInstall.sh
@@ -3,11 +3,12 @@ git pull
 yes | cp -arf issabelmodules/modules /var/www/html
 
 function callRequest(){
-cp -rf novoipagi /var/lib/asterisk/agi-bin
-chmod -R 777 /var/lib/asterisk/agi-bin/novoipagi
-query="REPLACE INTO miscdests (id,description,destdial) VALUES('102','callRequest','6668')"
-# mysql -hlocalhost -uroot -p$rootpw asterisk -e "$query"
-echo "**Queue callRequest Module Added." >> voiz-installation.log
+    cp -rf novoipagi /var/lib/asterisk/agi-bin
+    chmod -R 777 /var/lib/asterisk/agi-bin/novoipagi
+    query="REPLACE INTO miscdests (id,description,destdial) VALUES('102','callRequest','6668')"
+    # mysql -hlocalhost -uroot -p$rootpw asterisk -e "$query"
+    echo "**Queue callRequest Module Added." >> voiz-installation.log
+    echo "* * * * *  root /usr/bin/php -q /var/www/html/modules/novoip-callreq/cron.php" >> nano /etc/cron.d/novoip.cron
 }
 function featurecodes(){
 cp -rf customdialplan/extensions_voipiran_featurecodes.conf /etc/asterisk/
