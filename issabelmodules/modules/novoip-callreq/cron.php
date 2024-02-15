cd22e103a73be368d25f7c45216e32c20b6236b4
macbook <shayan.noje@gmail.com>
2024-02-15 17:54:14 +0330

    cr

--- issabelmodules/modules/novoip-callreq/cron.php
+++ issabelmodules/modules/novoip-callreq/cron.php
@@ -55,7 +55,7 @@ class CallRequest
         {
             $astman = new AGI_AsteriskManager();
             $astman->log_level = 0;
-            if (!$astman->connect("127.0.0.1", "admin" , "aaddmin")) {
+            if (!$astman->connect("127.0.0.1", "admin" , obtenerClaveAMIAdmin())) {
                 $this->errMsg = _tr('Error when connecting to Asterisk Manager');
                 return NULL;
             }
