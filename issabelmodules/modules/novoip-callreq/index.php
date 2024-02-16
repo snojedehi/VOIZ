1d77c22bee206bdaff9b2b8fea8e1d05e9753a0e
macbook <shayan.noje@gmail.com>
2024-02-16 18:44:58 +0330

    as

--- issabelmodules/modules/novoip-callreq/index.php
+++ issabelmodules/modules/novoip-callreq/index.php
@@ -73,7 +73,7 @@ class CallRequest
         $result = $pDB->genExec("
         INSERT INTO `asterisk`.`novoip_callrequests` ( `name`,`prefix`, `repeat`, `event`, `status`, `trunk`) VALUES (' $_POST[name]','$_POST[prefix]', '$_POST[repeat]', '2024-02-13 00:00:00', '$_POST[status]', '$_POST[trunk]');
         ");
-        $inID = $pDB->lastInsertId();
+        $inID = 2;
         
         $result = $pDB->genExec("
         INSERT INTO `asteriskcdrdb`.`novoip_callrequests_phones` (`id`, `number`, `repeat`, `status`, `callDate`, `uniqueID`, `CID`) VALUES (NULL, '09122389046', '0', 'wating', now(), '', '$inID')
