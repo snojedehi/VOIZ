f8354f2bd77629f73e7a626a70846b857dad3f89
macbook <shayan.noje@gmail.com>
2024-02-16 13:34:58 +0330

    agi

--- issabelmodules/modules/novoip-callreq/index.php
+++ issabelmodules/modules/novoip-callreq/index.php
@@ -161,7 +161,7 @@ class CallRequest
             $smarty->assign("novoip_data", $this->errMsg);
         }
         $smarty->assign("novoip_data", json_encode($queues));
-        // $this->asteriskCallto($astman);
+        $this->asteriskCallto($astman);
 
         $oForm    = new paloForm($smarty,array());
         $content  = $oForm->fetchForm("$local_templates_dir/form.tpl",_tr("Softphones"), array());
