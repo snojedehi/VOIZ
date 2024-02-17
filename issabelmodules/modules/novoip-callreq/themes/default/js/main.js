11aa8e1857ddd0f0e5823497190e499990614294
root <aaddminShayan22>
2024-02-17 15:51:48 +0330

    re

--- issabelmodules/modules/novoip-callreq/themes/default/js/main.js
+++ issabelmodules/modules/novoip-callreq/themes/default/js/main.js
@@ -1,13 +1,9 @@
 
 var editModal=function(id){
-    alert(123)
-    $('#editModal').on('show.bs.modal', function (event) {
-        var button = $(event.relatedTarget) // Button that triggered the modal
-        var recipient = button.data('whatever') // Extract info from data-* attributes
-        // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
-        // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
-        var modal = $(this)
-        modal.find('.modal-title').text('New message to ' + recipient)
-        modal.find('.modal-body input').val(recipient)
+
+    $.getJSON("/rest.php/novoip-callreq/Request/edit",{"eid":id}).then(function(res){
+        console.log(res)
+        $('#editModal').modal('show')
     })
+    
 }
