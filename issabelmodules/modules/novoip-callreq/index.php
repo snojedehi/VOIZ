6e0e86dd87c0cc8b9f33296b5796de43be4e1fe4
root <aaddminShayan22>
2024-02-17 17:23:30 +0330

    q

--- issabelmodules/modules/novoip-callreq/index.php
+++ issabelmodules/modules/novoip-callreq/index.php
@@ -78,8 +78,9 @@ class CallRequest
     private $errMsg = NULL;
 
     function addCall($pDB){
+        $status=$_POST[status]?1:0;
         $result = $pDB->genExec("
-        INSERT INTO `asteriskcdrdb`.`novoip_callrequests` ( `name`,`prefix`, `repeat`, `event`, `status`, `trunk`) VALUES (' $_POST[name]','$_POST[prefix]', '$_POST[repeat]', '2024-02-13 00:00:00', '$_POST[status]', '$_POST[trunk]');
+        INSERT INTO `asteriskcdrdb`.`novoip_callrequests` ( `name`,`prefix`, `repeat`, `event`, `status`, `trunk`) VALUES (' $_POST[name]','$_POST[prefix]', '$_POST[repeat]', '2024-02-13 00:00:00', '$status', '$_POST[trunk]');
         ");
 
         $inID = $pDB->getLastInsertId();
@@ -92,8 +93,9 @@ class CallRequest
 
     }
     function editCall($pDB){
+        $status=$_POST[status]?1:0;
         $result = $pDB->genExec("
-        UPDATE `novoip_callrequests` SET `name`='$_POST[name]',`prefix`='$_POST[prefix]',`repeat`='$_POST[repeat]',`event`='2024-02-13 00:00:00',`status`='$_POST[status]',`trunk`='$_POST[trunk]' WHERE id=$_POST[editReq]
+        UPDATE `novoip_callrequests` SET `name`='$_POST[name]',`prefix`='$_POST[prefix]',`repeat`='$_POST[repeat]',`event`='2024-02-13 00:00:00',`status`='$status',`trunk`='$_POST[trunk]' WHERE id=$_POST[editReq]
         ");
         
 
