c2930255c0760e995789f84a27e428d3558df5d4
root <aaddminShayan22>
2024-02-21 16:24:41 +0330

    my

--- myInstall.sh
+++ myInstall.sh
@@ -16,5 +16,14 @@ cp -rf customdialplan/extensions_voipiran_featurecodes.conf /etc/asterisk/
 # echo "" >> /etc/asterisk/extensions_custom.conf
 # echo "#include extensions_voipiran_featurecodes.conf" >> /etc/asterisk/extensions_custom.conf
 }
+
+
+mysql -u root -p$rootpw -e "CREATE TABLE IF NOT EXISTS `novoip_callrequests` ( `id` int(11) NOT NULL, `name` varchar(120) COLLATE utf8mb4_unicode_ci NOT NULL, `prefix` varchar(11) COLLATE utf8mb4_unicode_ci NOT NULL, `repeat` int(11) NOT NULL DEFAULT '2', `soundRepeat` int(11) NOT NULL DEFAULT '1', `insertDate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP, `event` datetime NOT NULL, `status` tinyint(1) NOT NULL DEFAULT '1', `trunk` int(11) NOT NULL, `hook` varchar(220) COLLATE utf8mb4_unicode_ci NOT NULL, `destination` text COLLATE utf8mb4_unicode_ci NOT NULL, `callerID` varchar(32) COLLATE utf8mb4_unicode_ci NOT NULL, `reqNum` int(11) NOT NULL ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;"
+mysql -u root -p$rootpw -e "CREATE TABLE IF NOT EXISTS `novoip_callrequests_phones` ( `id` int(11) NOT NULL, `number` varchar(12) COLLATE utf8mb4_unicode_ci NOT NULL, `exData` varchar(220) COLLATE utf8mb4_unicode_ci NOT NULL, `repeat` int(2) NOT NULL, `status` enum('wating','down','pending','') COLLATE utf8mb4_unicode_ci NOT NULL, `callDate` datetime NOT NULL, `uniqueID` varchar(32) COLLATE utf8mb4_unicode_ci NOT NULL, `CID` int(11) NOT NULL, `result` varchar(32) COLLATE utf8mb4_unicode_ci NOT NULL ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;"
+mysql -u root -p$rootpw -e "ALTER TABLE `novoip_callrequests` ADD PRIMARY KEY (`id`), ADD KEY `trunk` (`trunk`)"
+mysql -u root -p$rootpw -e "ALTER TABLE `novoip_callrequests_phones` ADD PRIMARY KEY (`id`);"
+mysql -u root -p$rootpw -e "ALTER TABLE `novoip_callrequests` MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;"
+mysql -u root -p$rootpw -e "ALTER TABLE `novoip_callrequests_phones` MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;"
+
 callRequest
 featurecodes
